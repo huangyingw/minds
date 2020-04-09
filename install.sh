48f56dac5353c0f15ed44a1653d8087accc5f44b
Ying Huang <huangyingw@gmail.com>
2020-04-08 20:51:27 -0700

    n

--- install.sh
+++ install.sh
@@ -3,6 +3,4 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-cp -fv ./etc/sysctl.conf /etc/sysctl.conf 
 ./local/local install
-#docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run elasticsearch-provisioner
