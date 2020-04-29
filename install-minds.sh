173ae41f2d6c624c8d22e7816154dfc5305b40b1
Ying Huang <huangyingw@gmail.com>
2020-04-29 12:23:09 -0400

    n

--- install-minds.sh
+++ install-minds.sh
@@ -3,6 +3,8 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
+rm engine/settings.php
 docker-compose down
-docker-compose build
-rm engine/settings.php ; docker-compose run installer
+docker-compose build minds_php \
+    && docker-compose build installer \
+    && docker-compose run installer
