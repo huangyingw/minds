83715bc7c5c08cb4a43ad3d25507e94dd1b609a3
Ying Huang <huangyingw@gmail.com>
2020-06-28 11:46:47 -0400

    n

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-#./local/local install
-rm engine/settings.php ; docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run installer
+./local/local install
+#rm engine/settings.php ; docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run installer
