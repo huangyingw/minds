02aacc38e8e159e4cbd4fed245174475c9eb0be4
Ying Huang <huangyingw@gmail.com>
2020-07-01 00:46:30 -0400

    n

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-#./local/local install --no-front
+docker-compose -f docker-compose.yml build minds_php
+./local/local install --no-front
 #rm engine/settings.php ; docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run installer
-docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run elasticsearch-provisioner 
+#docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run elasticsearch-provisioner
