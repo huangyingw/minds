b750196c95713aa5776e1c12623e3cb7dbb6d484
Ying Huang <huangyingw@gmail.com>
2020-07-10 12:45:50 -0400

    n

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
+sysctl -w vm.max_map_count=262144
 docker-compose -f docker-compose.yml build minds_php
 ./local/local install --no-front
 #rm engine/settings.php ; docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run installer
