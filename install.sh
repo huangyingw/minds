323022931a538294d1c00778ae9193b57ecc3eb8
Ying Huang <huangyingw@gmail.com>
2020-11-19 21:50:39 -0800

    n

--- install.sh
+++ install.sh
@@ -8,3 +8,18 @@ sysctl -w vm.max_map_count=262144
 #./local/local install --no-front
 #rm engine/settings.php ; docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run installer
 docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run elasticsearch-provisioner
+
+# https://developers.minds.com/docs/getting-started/installation/
+npm install -g gulp
+sysctl -w vm.max_map_count=262144
+#docker-compose -f docker-compose.yml build minds_php
+./local/local install --no-front
+# ./local/local up
+#rm engine/settings.php ; docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run installer
+#docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run elasticsearch-provisioner
+# ./local/front-build
+# ./local/ssr-build
+# ./local/ssr-serve
+# ./front/install.sh
+# ./front/serve.sh
+# ./local/front-build
