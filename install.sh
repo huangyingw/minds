df3dd62dba6674b34b8fbebad350fc6e0aa2d5a6
Ying Huang <huangyingw@gmail.com>
2020-11-19 21:32:59 -0800

    n

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-docker-compose -f docker-compose.yml build minds_php
-./local/local install --no-front
+sysctl -w vm.max_map_count=262144
+#docker-compose -f docker-compose.yml build minds_php
+#./local/local install --no-front
 #rm engine/settings.php ; docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run installer
-#docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run elasticsearch-provisioner
+docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run elasticsearch-provisioner
