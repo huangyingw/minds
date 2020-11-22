b449728531f28a61e60d9435061d18d3c5ab9909
Ying Huang <huangyingw@gmail.com>
2020-11-21 19:23:38 -0800

    n

--- install.sh
+++ install.sh
@@ -10,10 +10,9 @@ sysctl -w vm.max_map_count=262144
 docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run elasticsearch-provisioner
 
 # https://developers.minds.com/docs/getting-started/installation/
-npm install -g gulp
-sysctl -w vm.max_map_count=262144
+#npm install -g gulp
 #docker-compose -f docker-compose.yml build minds_php
-./local/local install --no-front
+#./local/local install --no-front
 # ./local/local up
 #rm engine/settings.php ; docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run installer
 #docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run elasticsearch-provisioner
