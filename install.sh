fc90d84d3c63ae4c097228fd2a6cae4417f5db16
Ying Huang <huangyingw@gmail.com>
2020-11-26 15:17:48 -0800

    n

--- install.sh
+++ install.sh
@@ -7,20 +7,15 @@ sysctl -w vm.max_map_count=262144
 docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml build
 #docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml up elasticsearch
 #docker-compose -f docker-compose.yml build minds_php
-#./local/local install --no-front
 #rm engine/settings.php ; docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run installer
-#docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml build
 #docker-compose -f docker-compose.yml up elasticsearch
 #docker-compose up elasticsearch
 #docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run elasticsearch-provisioner
 
 # https://developers.minds.com/docs/getting-started/installation/
-#npm install -g gulp
-#docker-compose -f docker-compose.yml build minds_php
-#./local/local install --no-front
+# npm install -g gulp
+# ./local/local install --no-front
 # ./local/local up
-#rm engine/settings.php ; docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run installer
-#docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run elasticsearch-provisioner
 # ./local/front-build
 # ./local/ssr-build
 # ./local/ssr-serve
