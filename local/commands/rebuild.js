4015b20268b22fb34cfa65ad1c94a05a4db34c4a
Ying Huang <huangyingw@gmail.com>
2020-11-26 15:44:30 -0800

    n

--- local/commands/rebuild.js
+++ local/commands/rebuild.js
@@ -12,6 +12,7 @@ module.exports.handler = async argv => {
   const tasks = new Listr([
     require('../tasks/stop'),
     require('../tasks/build-containers'),
+    require('../tasks/build-front'),
   ], {
     renderer
   });
