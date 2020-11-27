94181dec645699b9b6723c408ce860a1fbf8de8b
Ying Huang <huangyingw@gmail.com>
2020-11-26 21:41:01 -0800

    n

--- debug.sh
+++ debug.sh
@@ -8,6 +8,7 @@ docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml stop cas
 rm -fr /var/lib/docker/volumes/minds_cassandra-data/_data/*
 # docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml up -d cassandra
 rm engine/settings.php ; docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml run installer
+docker-compose logs nginx php-fpm
 # docker-compose up cassandra
 # docker inspect minds_cassandra_1 > g.findresult
 # docker-compose -f docker-compose.yml -f docker-compose.with-phpspec.yml build
