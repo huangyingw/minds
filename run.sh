3af6df074a0f921d6e27258d2a96acf9bbe9be53
Ying Huang <huangyingw@gmail.com>
2020-11-26 20:41:43 -0800

    n

--- run.sh
+++ run.sh
@@ -4,8 +4,10 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 #./local/local build
+sysctl -w vm.max_map_count=262144
+rm -fr /var/lib/docker/volumes/minds_cassandra-data/_data/*
 ./local/local install
 ./local/local up
-./local/front-build &
-./local/ssr-build &
-./local/ssr-serve &
+# ./local/front-build
+# ./local/ssr-build
+# ./local/ssr-serve
