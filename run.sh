95fef9e88842cb48ecc85cae74bacaaf8b190a9e
Ying Huang <huangyingw@gmail.com>
2020-11-27 20:15:27 -0800

    n

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-#./local/local build
 sysctl -w vm.max_map_count=262144
-rm -fr /var/lib/docker/volumes/minds_cassandra-data/_data/*
+./local/local build &
+docker volume prune -f
 ./local/local install
 ./local/local up
 # ./local/front-build
